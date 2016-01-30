0e0a940bba42c9dd5225e243b849a229a631b07f
Tanner Jones <tannerjones10@gmail.com>
2016-01-29 22:30:59 -0500

    Add files to include path
    Adds the null_board, column, and container classes to the load path.

--- lib/sudoku.rb
+++ lib/sudoku.rb
@@ -1,5 +1,8 @@
 require 'sudoku/game'
 require 'sudoku/board'
+require 'sudoku/null_board'
 require 'sudoku/square'
+require 'sudoku/container'
+require 'sudoku/column'
 require 'sudoku/io'
 require 'sudoku/errors'
