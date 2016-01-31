0a5ce0605ed0ed6121528d09cc11afd1be7c54cb
Tanner Jones <tannerjones10@gmail.com>
2016-01-30 22:51:55 -0500

    Update Sudoku::Container module
    Turns out, ruby mixins don't like instance variables. Therefore, we'll
    interact with the member_squares array through get_members and
    set_member methods. We'll also use a calc_members method to put the
    functionality of deciding which squares should belong to a certain
    container with said container, not with the board object.

--- lib/sudoku/container.rb
+++ lib/sudoku/container.rb
@@ -1,5 +1,16 @@
 module Sudoku
 module Container
-  @member_squares = []
+
+  def calc_members
+    raise NotImplementedError, "Containers must implement calc_members"
+  end
+
+  def get_members
+    raise NotImplementedError, "Containers must implement get_members"
+  end
+
+  def set_member( args={} )
+    raise NotImplementedError, "Containers must implement set_member"
+  end
 end
 end
