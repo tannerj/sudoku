e29c5b6435cb32a6d298911d8d462f9d98e7b588
Tanner Jones <tannerjones10@gmail.com>
2016-02-17 23:26:24 -0500

    Remove redundant Sudoku namespace from board_spec.
    The Board spec is located inside the Sudoku module, no need for a
    Sudoku:: namespace when initializing objects.

--- spec/sudoku/board_spec.rb
+++ spec/sudoku/board_spec.rb
@@ -4,11 +4,11 @@ module Sudoku
 RSpec.describe Board do
 
   describe "#create" do
-    let(:square) { $square = Sudoku::Square }
-    let(:game) { $game = Sudoku::Game }
+    let(:square) { $square = Square }
+    let(:game) { $game = Game }
     let(:square_values) { (1..81).to_a.map! {|x| x = "0"}.join }
     let(:board) do
-      $board = Sudoku::Board.create(
+      $board = Board.create(
         square_values: square_values,
         square: square,
         game: game
@@ -30,12 +30,12 @@ RSpec.describe Board do
   describe "#square_position" do
     context "with correct parameter" do
       it "should return a corrisponding row, column pair for given value." do
-        @row_column_ref = Sudoku::Board.square_position(id: 1)
+        @row_column_ref = Board.square_position(id: 1)
         expect(@row_column_ref).to eq("r1c1")
       end
 
       it "should compute last items in a row correctly" do
-        @row_column_ref = Sudoku::Board.square_position(id: 18) 
+        @row_column_ref = Board.square_position(id: 18) 
         expect(@row_column_ref).to eq("r2c9")
       end
     end
@@ -43,13 +43,13 @@ RSpec.describe Board do
     context "with incorrect parameters" do
       it "should raise an argument error if the id is less than 1." do
         expect{
-          @row_column_ref = Sudoku::Board.square_position(id: 0)
+          @row_column_ref = Board.square_position(id: 0)
         }.to raise_error(ArgumentError)
       end
 
       it "should raise an argument error if the id is greater than 81" do
         expect{
-          @row_column_ref = Sudoku::Board.square_position(id: 82)
+          @row_column_ref = Board.square_position(id: 82)
         }.to raise_error(ArgumentError)
       end
     end
@@ -57,10 +57,10 @@ RSpec.describe Board do
 
   describe "#update_square" do
     let(:square_values) { (1..81).to_a.map! {|x| x = "0"}.join }
-    let(:square) { Sudoku::Square }
-    let(:game) { Sudoku::Game }
+    let(:square) { Square }
+    let(:game) { Game }
     let(:board) do
-      Sudoku::Board.create(
+      Board.create(
         square_values: square_values,
         square: square,
         game: game
@@ -89,35 +89,18 @@ RSpec.describe Board do
     it "should update a given square's peers' possible_values with square's value."
   end
 
-  describe "#find_peers" do
-    let(:peer_ids) do
-      $peer_ids = [1,2,3,4,5,6,7,8,9,10,11,12,19,20,21,28,37,46,55,64,73]
-    end
-    let(:square) { Sudoku::Square.new id: 1, value: nil }
-    let(:updated_square_value) { 1 }
-    let(:square_values) { (1..81).to_a.map! {|x| x = "0" }.join }
-    let(:game) { Sudoku::Game }
-    let(:board) do
-      Sudoku::Board.create(
-        square_values: square_values,
-        square: square,
-        game: game
-      )
-    end
-    xit "should return an array of peer id's that correspond to given square id." do
-      board
-      expect(board.find_peers(square)).to match_array(peer_ids)
-    end
-  end
-
   describe "#set_container_members" do
-    let(:board) { Sudoku::Board.create(
+    let(:board) { Board.create(
         square_values: (1..81).to_a.map! { |x| x = "0" }.join
       )
     }
     it "sets container.member_squares" do
       board
-      container = Sudoku::Container.new id: 1, member_calculator: Sudoku::MemberCalculator::Column.new, board: board
+      container = Container.new( 
+        id: 1, 
+        member_calculator: MemberCalculator::Column.new, 
+        board: board
+      )
       expected_members = board.squares.select do |square|
         [1, 10, 19, 28, 37, 46, 55, 64, 73].include? square.id
       end
