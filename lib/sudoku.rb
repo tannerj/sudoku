751c6797ea541a950d7b0a255262ab3afc22e3a8
Joel Tanner Jones <jonesjoe@ecu.edu>
2016-11-18 16:55:41 -0500

    Add example to lib/sudoku

--- lib/sudoku.rb
+++ lib/sudoku.rb
@@ -4,4 +4,17 @@ require_relative 'sudoku/units'
 require_relative 'sudoku/io'
 
 board = Sudoku::Board.new
-puts Sudoku::IO::CLI.render_board board
+game = "010020300004005060070000008006900070000100002030048000500006040000800106008000000"
+game_array = []
+
+game.each_char do |char|
+  game_array << char
+end
+
+board.squares.each do |index, square|
+  value = game_array.shift
+  square.value = value if value != 0
+end
+
+board.squares["G7"].value = 8
+puts Sudoku::IO::CLI.render_board board: board
